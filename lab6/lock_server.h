ea9af35b66181a6a26c01e2756f8491805b206f4
ljw9609 <473100319@qq.com>
2018-01-12 09:35:38 +0800

    solution for lab6

---
+++ lab6/lock_server.h
@@ -0,0 +1,38 @@
+// this is the lock server
+// the lock client has a similar interface
+
+#ifndef lock_server_h
+#define lock_server_h
+
+#include <string>
+#include <map>
+#include "lock_protocol.h"
+#include "lock_client.h"
+#include "rpc.h" 
+using namespace std;
+
+class lock_server {
+
+ protected:
+  int nacquire;
+  map<lock_protocol::lockid_t,bool> lock_status; //status,true = locked,false = unlocked
+
+  static pthread_mutex_t mutex;
+  static pthread_cond_t cond;
+
+ public:
+  lock_server();
+  ~lock_server() {};
+  lock_protocol::status stat(int clt, lock_protocol::lockid_t lid, int &);
+  lock_protocol::status acquire(int clt, lock_protocol::lockid_t lid, int &);
+  lock_protocol::status release(int clt, lock_protocol::lockid_t lid, int &);
+};
+
+#endif 
+
+
+
+
+
+
+
